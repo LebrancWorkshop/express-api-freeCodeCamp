0d095435f8f12c00ff92ff6ed65e9308928b9892
lebrancconvas <lebrancconvas@gmail.com>
2023-04-21 16:27:40 +0700

    :tada: Init.

---
+++ myApp.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
